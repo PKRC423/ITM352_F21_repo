4fc661f14279bf7eb3f68d910d749b0a9a98ff46
PKRC423 <pkrc@hawaii.edu>
2021-11-14 15:45:52 -1000

    Assignment 1 Update

--- Rivera-Concannon_Peter_Assignment1/server.js
+++ Rivera-Concannon_Peter_Assignment1/server.js
@@ -3,7 +3,7 @@ Statement about what this page is and giving credit to me for making it
 */
 
 var products = require('./products');
-products.forEach( (prod,i) => {prod.total_sold = 0});
+products.forEach( (prod,i) => {prod.total_sold = 0}); //so each time a "bin" is created it is assigned a number.
 var fs = require('fs');
 var express = require('express');
 const { checkServerIdentity } = require('tls');
@@ -19,20 +19,6 @@ app.all('*', function (request, response, next) {
 //So Express can decode the body of an HTTP request since by default Express cannot do that.
 app.use(express.urlencoded({extended: true}));
 
-// process purchase request (validate quantities, check quantity available)
-        //This function checks if the input is a non-negative integer and if there are more than or equal to 5 tickets of the same typ are 
-    function checkInt(inputStr, returnErr = false) {
-       errors = []; //No errors yet hopefully
-        if (Number(inputStr) != inputStr) {
-            errors.push("Not a Valid Quantity")
-        } else if(inputStr < 0) {
-            errors.push('Not a Valid Quantity')
-            } else {
-                if (parseInt(inputStr) != inputStr) errors.push('Not a Valid Quanity');
-                if (inputStr >= 5) errors.push('Too many Tickets Allowed by 1 Party');
-            }
-        return returnErr ? errors : (errors.length == 0);
-        }
 
 // route all other GET requests to files in public 
 app.use(express.static('./public'));
@@ -46,22 +32,26 @@ app.get("/products.js", function (request, response, next) {
     var products = `${JSON.stringify(products)};`;
     response.send(products);
 });
-    
-       //Refrenced from the Lab13 Ex5
-app.post("/process_invoice_form", function (request, response, next) {
-    let POST = request.body;
-    let section =products[0]['section_num'];
-    let section_price=products[0]['price'];
 
-        if(typeof POST['quantity_textbox'] != 'undefined') {
-            let quantity = POST['quantity_textbox'];
-            if (checkInt(quantity)) {
-                products[0]['total_sold'] += Number(quantity);
-                response.redirect('/______' + quantity);
-            } else {
-                response.redirect('/______' +quantity);
-            }
+    // process purchase request (validate quantities, check quantity available)
+//This function checks if the input is a non-negative integer and if there are more than or equal to 5 tickets of the same type are purchased.
+function checkInt(inputStr, returnErr = false) {
+    errors = []; //No errors yet hopefully
+    if (Number(inputStr) != inputStr) {        //Checks if it is a number
+        errors.push("Not a Valid Quantity")
+    } else if(inputStr < 0) {                  //Checks if it's a negative value
+        errors.push('Not a Valid Quantity')
+        } else {
+             if (parseInt(inputStr) != inputStr) errors.push('Not a Valid Quanity'); //Checks if it has decimal values
+            if (inputStr >= 5) errors.push('Too many Tickets Allowed by 1 Party'); //Checks if ober 5 ticekts are being bought from that section.
         }
+    return returnErr ? errors : (errors.length == 0);
+    }
+
+
+       //Referenced from the Lab13 Ex5 to process the invoice form.
+app.post("/process_invoice_form", function (request, response, next) {
+    
     
         
     //Referenced from Invoice 4
@@ -85,14 +75,14 @@ app.get("/UHManoaFootballTickets", function (request, response) {
             for (i = 0; i < products.length; i++) {            
                 str += `
                 <section style="text-align: center">
-                <hr/>
+                <hr>
                 <h1>Sections: ${products[i].section_num}</h1>
 
                 <h2>Ticket price: <br> $${products[i].price}</h2>
 
                 <h3> Quantity </h3>
 
-                <input type"text" placeholder="0" name = "quantity" onkeyup = "checkInt(this);'>
+                <input type"text" placeholder="0" name = "quantity${i}" onkeyup = "checkInt(this);"'>
 
                 <h2>There are: ${products[i].qty_available} Seats Available</h2>
                 </section>
